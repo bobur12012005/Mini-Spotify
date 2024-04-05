13fffdabab2157db260d4c9209e6dc1f8c210c22
Bobur Makhmadkulov <boburmakhmadkulov05@gmail.com>
2024-04-05 16:38:44 +0500

    first commit

---
+++ JavaScript/db.js
@@ -0,0 +1,212 @@
+export let songs = [
+    {
+        id: 1,
+        title: 'Godzilla',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2020 + ' years ago',
+        url: './songs/EMINEM - Godzilla.mp3',
+        image: './Images/godzilla.jpg',
+        duration: '4:26',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 2,
+        title: 'Lose Yourself',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2002 + ' years ago',
+        url: './songs/EMINEM - Lose Yourself.mp3',
+        image: './Images/lose yourself.jpg',
+        duration: '5:26',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 3,
+        title: 'Mockingbird',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2004 + ' years ago',
+        url: './songs/EMINEM - Mockingbird.mp3',
+        image: './Images/mockingbird.jpg',
+        duration: '4:11',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 4,
+        title: 'Sing For The Moment',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2002 + ' years ago',
+        url: './songs/EMINEM - Sing For The Moment.mp3',
+        image: './Images/sing for the moment.jpg',
+        duration: '5:40',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 5,
+        title: 'Smack That',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2006 + ' years ago',
+        url: './songs/EMINEM - Smack That.mp3',
+        image: './Images/smack that.jpg',
+        duration: '3:33',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 6,
+        title: 'The Real Slim Shady',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2000 + ' years ago',
+        url: './songs/EMINEM - The Real Slim Shady.mp3',
+        image: './Images/the real slim shady.jpg',
+        duration: '4:44',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 7,
+        title: 'Venom',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2018 + ' years ago',
+        url: './songs/EMINEM - Venom.mp3',
+        image: './Images/venom.jpg',
+        duration: '4:29',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 8,
+        title: 'When I`m Gone',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2005 + ' years ago',
+        url: "./songs/EMINEM - When I'm Gone.mp3",
+        image: './Images/when im gone.jpg',
+        duration: '4:41',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 9,
+        title: 'Gnat',
+        album: 'Eminem`s album',
+        release: new Date().getFullYear() - 2020 + ' years ago',
+        url: './songs/EMINEM - Gnat.mp3',
+        image: './Images/gnat.jpg',
+        duration: '3:44',
+        singer: {
+            name: 'Eminem',
+            fullname: 'Marshal Bruce Mathers III',
+            birthday: '17th October 1972'
+        }
+    },
+    {
+        id: 10,
+        title: 'Bad Habits',
+        album: 'Ed Sheeran`s album',
+        release: new Date().getFullYear() - 2021 + ' years ago',
+        url: './songs/Ed Sheeran - Bad Habits.mp3',
+        image: './Images/bad habits.jpg',
+        duration: '3:51',
+        singer: {
+            name: 'Ed Sheeran',
+            fullname: 'Edward Christopher Sheeran',
+            birthday: '17th February 1991'
+        }
+    },
+    {
+        id: 11,
+        title: 'Perfect',
+        album: 'Ed Sheeran`s album',
+        release: new Date().getFullYear() - 2017 + ' years ago',
+        url: './songs/Ed Sheeran - Perfect.mp3',
+        image: './Images/perfect.jpg',
+        duration: '4:23',
+        singer: {
+            name: 'Ed Sheeran',
+            fullname: 'Edward Christopher Sheeran',
+            birthday: '17th February 1991'
+        }
+    },
+    {
+        id: 12,
+        title: 'Shape Of You',
+        album: 'Ed Sheeran`s album',
+        release: new Date().getFullYear() - 2017 + ' years ago',
+        url: './songs/Ed Sheeran - Shape Of You.mp3',
+        image: './Images/shape of you.jpg',
+        duration: '3:46',
+        singer: {
+            name: 'Ed Sheeran',
+            fullname: 'Edward Christopher Sheeran',
+            birthday: '17th February 1991'
+        }
+    },
+    {
+        id: 13,
+        title: 'The Search',
+        album: 'NF`s album',
+        release: new Date().getFullYear() - 2019 + ' years ago',
+        url: './songs/NF - The Search.mp3',
+        image: '../Images/the search.jpg',
+        duration: '3:15',
+        singer: {
+            name: 'NF',
+            fullname: 'Nathan John Feuerstein',
+            birthday: '30th March 1991'
+        }
+    },
+    {
+        id: 14,
+        title: 'Paralyzed',
+        album: 'NF`s album',
+        release: new Date().getFullYear() - 2015 + ' years ago',
+        url: './songs/NF - Paralyzed.mp3',
+        image: '../Images/paralyzed.jpg',
+        duration: '4:30',
+        singer: {
+            name: 'NF',
+            fullname: 'Nathan John Feuerstein',
+            birthday: '30th March 1991'
+        }
+    },
+    {
+        id: 15,
+        title: 'When I Grow Up',
+        album: 'NF`s album',
+        release: new Date().getFullYear() - 2019 + ' years ago',
+        url: './songs/NF - When I Grow Up.mp3',
+        image: '../Images/when i grow up.jpg',
+        duration: '3:16',
+        singer: {
+            name: 'NF',
+            fullname: 'Nathan John Feuerstein',
+            birthday: '30th March 1991'
+        }
+    }
+]
---
+++ JavaScript/index.js
@@ -0,0 +1,118 @@
+import { songs } from "./db.js"
+
+let audio = document.getElementById('audio')
+let container = document.querySelector('.elements')
+let liked = []
+let nameSong = document.querySelector('.playerData span')
+
+function reload(arr, place) {
+    place.innerHTML = ''
+
+    for (let item of arr) {
+        let song = document.createElement('div')
+        let more = document.createElement('div')
+        let number = document.createElement('span')
+        let song_img = document.createElement('img')
+        let TS = document.createElement('div')
+        let song_title = document.createElement('span')
+        let song_singer = document.createElement('span')
+        let album = document.createElement('div')
+        let album_txt = document.createElement('span')
+        let release = document.createElement('div')
+        let release_txt = document.createElement('span')
+        let actions = document.createElement('div')
+        let like_btn = document.createElement('button')
+        let like_img = document.createElement('img')
+        let duration = document.createElement('span')
+        let play_btn = document.createElement('button')
+        let play_icon = document.createElement('img')
+
+        song.classList.add('song')
+        more.classList.add('more')
+        number.classList.add('number')
+        song_img.classList.add('song_img')
+        TS.classList.add('TS')
+        song_title.classList.add('song_title')
+        song_singer.classList.add('song_singer')
+        album.classList.add('album')
+        release.classList.add('release')
+        actions.classList.add('actions')
+        like_btn.classList.add('like_btn')
+        play_btn.classList.add('play_btn')
+
+        number.innerHTML = item.id
+        song_title.innerHTML = item.title
+        song_singer.innerHTML = item.singer.name
+        album_txt.innerHTML = item.album
+        release_txt.innerHTML = item.release
+        duration.innerHTML = item.duration
+        nameSong.innerHTML = item.title
+
+        song_img.src = item.image
+        like_img.src = './Icons/heart.png'
+
+        container.append(song)
+        song.append(more, album, release, actions)
+        more.append(number, song_img, TS)
+        TS.append(song_title, song_singer)
+        album.append(album_txt)
+        release.append(release_txt)
+        actions.append(duration, like_btn)
+        like_btn.append(like_img)
+        play_btn.append(play_icon)
+
+        song.onmouseenter = () => {
+            number.remove()
+            more.append(play_btn, song_img, TS)
+            play_icon.src = './Icons/play.png'
+
+            play_btn.onclick = () => {
+                document.querySelectorAll('.song').forEach((song) => {
+                    song.querySelector('.TS').style.color = ''
+                    song.querySelector('.album').style.color = ''
+                    song.querySelector('.release').style.color = ''
+                    song.querySelector('.actions').style.color = ''
+                })
+
+                audio.src = item.url
+                play_icon.src = './Icons/pause.png'
+                audio.play()
+                
+                TS.style.color = '#00FF00'
+                album.style.color = '#00FF00'
+                release.style.color = '#00FF00'
+                actions.style.color = '#00FF00'
+                
+                if (document.querySelector('.TS').style.color === 'white') {
+                    play_icon.src = './Icons/play.png'
+                } else if (document.querySelector('.TS').style.color === '#00FF00') {
+                    play_icon.src = './Icons/pause.png'
+                }
+            }
+        }
+
+        song.onmouseleave = () => {
+            play_btn.remove()
+            more.append(number, song_img, TS)
+        }
+
+        like_btn.onclick = () => {
+            let index = liked.findIndex((likedItem) => likedItem.id === item.id)
+
+            if (!like_btn.classList.contains('like_active')) {
+                like_img.src = './Icons/heartFull.png'
+                like_btn.classList.add('like_active')
+                liked.push(item)
+            } else {
+                like_img.src = './Icons/heart.png'
+                like_btn.classList.remove('like_active')
+                if (index !== -1) {
+                    liked.splice(index, 1)
+                }
+            }
+            console.log(liked)
+        }
+    }
+}
+
+reload(songs, container)
